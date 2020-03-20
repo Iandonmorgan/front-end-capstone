9f05f6ffeab6fe9d280523a064838be38e40e99a
Landon Morgan <morganlandon@gmail.com>
2020-03-19 20:53:25 -0500

    dashboard ordering projects by date

--- src/components/dashboard/DashboardProjectList.js
+++ src/components/dashboard/DashboardProjectList.js
@@ -1,21 +1,32 @@
 import React, { useState, useEffect } from "react";
 import APIManager from "../../modules/APIManager";
 
-let statusArray = [];
-
 const DashboardProjectList = (props) => {
     const [artistProject, setArtistProject] = useState([]);
+    const [statusArray, setStatusArray] = useState([]);
 
     const getArtistProjects = (props) => {
         let artistProjects = [];
         APIManager.getAllWithExpand("artistProjects", "project").then(artistProjectz => {
-            artistProjects = artistProjectz.filter(project => project.artistId === props.artist.id);
+            artistProjects = artistProjectz.sort(function(a, b){return new Date(a.project.expectedCompletion) - new Date (b.project.expectedCompletion)}).filter(project => project.artistId === props.artist.id);
             setArtistProject(artistProjects);
+        });
+    };
+    
+    const getStatuses = () => {
+        let statuses = [];
+        const statusFunction = (status) => {
+            return status.name
+        }
+        APIManager.getAll("statuses").then(status => {
+            statuses = status.map(statusFunction);
+            setStatusArray(statuses);
         });
     };
 
     useEffect(() => {
         getArtistProjects(props);
+        getStatuses();
     }, []);
 
     return (
@@ -25,14 +36,14 @@ const DashboardProjectList = (props) => {
                     <div className="dashboardProjectItemName">
                         {(artistProject.project.name)}
                     </div>
-                    <div className="dashboardProjectItemDetails">
-                        {(artistProject.project.details)}
+                    <div className="dashboardProjectItemDescription">
+                        {(artistProject.project.description)}
                     </div>
                     <div className="dashboardProjectItemDeadline">
                         {(artistProject.project.expectedCompletion)}
                     </div>
                     <div className="dashboardProjectItemStatus">
-                        {(artistProject.project.statusId)}
+                        {(statusArray[artistProject.project.statusId - 1])}
                     </div>
                     </div>
                 )
