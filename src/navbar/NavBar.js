dca9a6b511de608daa34bfdf4aeb65921161c9ee
Landon Morgan <morganlandon@gmail.com>
2020-03-22 12:04:23 -0500

    trying to fix dependencies issues with navbar

--- src/navbar/NavBar.js
+++ src/navbar/NavBar.js
@@ -1,7 +1,4 @@
 import React from "react";
-import { Navbar, Nav } from 'react-bootstrap';
-import { withRouter } from "react-router-dom";
-
 
 const NavBar = props => {
   const clearUser = () => {
@@ -13,20 +10,15 @@ const NavBar = props => {
     props.history.push("/");
   };
   return (
-    <Navbar bg="dark" variant="dark" expand="md" sticky="top">
-      <Navbar.Brand href="/">Commissioner Mordan</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="/artists">Artists</Nav.Link>
-          <Nav.Link href="/projects">Projects</Nav.Link>
-          <Nav.Link href="/contacts" className="disabled">Contacts</Nav.Link>
-          <Nav.Link href="/shoots" className="disabled">Shoots</Nav.Link>
-          <Nav.Link href="/edits" className="disabled">Edits</Nav.Link>
-          <Nav.Link onClick={handleLogout} href="/">Logout</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <>
+      <div href="/">Commissioner Mordan</div>
+      <a href="/artists">Artists</a>
+      <a href="/projects">Projects</a>
+      <div className="disabled">Contacts</div>
+      <div className="disabled">Shoots</div>
+      <div className="disabled">Edits</div>
+      <a onClick={handleLogout} href="/">Logout</a>
+    </>
   );
 };
-export default withRouter(NavBar);
+export default NavBar;
